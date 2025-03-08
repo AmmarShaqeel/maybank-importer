4da5615e0313ca28e7c73aa0f232d8bdbe7fe37f
Ammar <17486328+AmmarShaqeel@users.noreply.github.com>
2025-03-08 11:05:29 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     install_requires=[
         "beancount>=3.0.0",
         "python-dateutil>=2.8.0",
-         "python-poppler>=0.4.1"",
+         "python-poppler>=0.4.1",
         "beangulp>=0.2.0",
     ],
     python_requires=">=3.7",
